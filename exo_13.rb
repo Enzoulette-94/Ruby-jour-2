e63dc1df2a0bb2e84e33a3d3ccbb04ea51ae3b60
Ton nom d'utilisateur github <enzonatali75@gmail.com>
2025-09-09 13:52:20 +0200

    Maj 2

--- exo_13.rb
+++ exo_13.rb
@@ -1,5 +1,13 @@
-51.times do |i|
-  puts "jeand.dupont.#{i}@email.fr"
-end
 
-#oublie pas d'encadrer le i a chaque fois 
+mail = []
+
+51.times do |i|
+#oublie pas d'encadrer le i a chaque fois
+mail << "jean.dupont.#{i}@email.fr"
+end 
+
+# << inserer dans le tableau
+
+mail.each do |ligne| # each uniquement pour tableau 
+  puts ligne
+end
--- exo_17.rb
+++ exo_17.rb
@@ -4,6 +4,7 @@ number_étages = gets.chomp.to_i
 if number_étages > 25 # cas ou c'est superieur à 25
    puts "désolé ma pyramide est pas assez grande"
 else
+    
 number_étages <= 25
     1.upto(number_étages) do |i|
     espace = " " * (number_étages - i)  
