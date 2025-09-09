898f1489845e3a91e6f2cdeb3edecaf695b62596
Ton nom d'utilisateur github <enzonatali75@gmail.com>
2025-09-09 13:56:34 +0200

    Maj 3

--- exo_14.rb
+++ exo_14.rb
@@ -1,9 +1,14 @@
+
+mail = []
+
 51.times do |i|
 
  if i.even?
   puts "jeand.dupont.#{i}@email.fr"
 
 #pair c'est even et impair c'est odd
-
+mail.each do |ligne| # each uniquement pour tableau 
+  puts ligne
 end
+end 
 end
